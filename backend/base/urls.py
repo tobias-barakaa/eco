652d2d2d1bc69decbd138eb8513cd3925f59323b
tobias-barakaa <ngairato@gmail.com>
2024-03-23 13:27:10 +0300

    django function

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="routes"),
-    path('products/<str:pk>/', views.getProduct, name="routes")
+    path('products/<str:pk>/', views.getProduct, name="routes"),
+    
+    # users
+    
+    path('users/profile/', views.getUserProfile, name="user-profile")
+    
     
     
 ]
