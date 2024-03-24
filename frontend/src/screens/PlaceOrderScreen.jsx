d3b42c3a9610348a9b6bc2f4bea757aa6c45a493
tobias-barakaa <ngairato@gmail.com>
2024-03-24 20:36:37 +0300

    more steps to finish

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,10 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 
 const PlaceOrderScreen = () => {
+    const navigate = useNavigate();
+    const cart = useSelector((state) => state.cart);
+
+    useEffect(() => {
+        if (!cart.paymentMethod) {
+            navigate('/payment')
+        } else if (!cart.paymentMethod) {
+            navigate('/shipping')
+        }
+    }, [cart.paymentMethod, navigate, cart.shippingAddress])
     
   return (
     <div>
-      sfdf
+      
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>Column</Col>
+        <Col md={4}>Column</Col>
+
+      </Row>
     </div>
   )
 }
