001009ce0cbf321d024f4ebe55a3deaac9ca337f
tobias-barakaa <ngairato@gmail.com>
2024-03-26 19:04:28 +0300

    master delta

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -4,8 +4,12 @@ from base.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='orders-add'),
+    path('myorders/', views.getMyOrders, name='myOrders'),
+    
     path('<str:pk>/', views.getOrderById, name='user-add'),
     path('<str:pk>/pay', views.updateOrderToPaid, name='pay'),
+    
+    
     
     
     
