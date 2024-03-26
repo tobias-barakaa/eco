677ba5ac65bd9f8e49c27f4eca1fc4caee630c3f
tobias-barakaa <ngairato@gmail.com>
2024-03-26 19:37:31 +0300

    admin deal

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -11,6 +11,15 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name="user-profile"),
     
     path('', views.getUsers, name="users"),
+    path('profile/update/', views.updateUserProfile, name="user-profile-update"),
+    
+    path('delete/<str:pk>/', views.deleteUser, name="users delete"),
+    
+    path('update/<str:pk>/', views.updateUser, name="user-update"),
+    path('<str:pk>/', views.getUserById, name="user"),
+    
+    
+    
     
     
 ]
--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -44,6 +44,26 @@ def registerUser(request):
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUserProfile(request):
+    user = request.user
+    serializer = UserSerializerWithToken(user, many=False)
+    
+    data = request.data
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+    
+    user.save()
+    
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
@@ -57,4 +77,41 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request, pk):
+    user = User.objects.get(pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteUser(request, pk):
+    userForDeletion = User.objects.get(id=pk)
+    userForDeletion.delete()
+    return Response('User was deleted')
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    
+    data = request.data
+    
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+       
+
+    user.save()
+    serializer = UserSerializer(user, many=False)
+    
+    
     return Response(serializer.data)
